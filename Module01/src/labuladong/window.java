f61de95b56b9a1e18fc2e9f9df4a8c082d0605f4
cqq <qingqingchen680@gmail.com>
2022-05-02 19:58:56 +0800

    第一次试一下更新

--- Module01/src/labuladong/window.java
+++ Module01/src/labuladong/window.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class window {
     public static void main(String[] args) {
         Solution76 solution =new Solution76();
-        
+
     }
 }
 
@@ -61,5 +61,58 @@ class Solution76 {
             }
         }
         return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
+}
+
+/**
+ * 567. 字符串的排列
+ * 给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。如果是，返回 true ；否则，返回 false 。
+ *
+ * 换句话说，s1 的排列之一是 s2 的 子串 。
+ */
+class Solution {
+    public boolean checkInclusion(String t, String s) {
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+        int left = 0, right = 0;
+        int valid = 0;
+        // 记录最小覆盖子串的起始索引及长度
+//        int start = 0, len = Integer.MAX_VALUE;
+
+        while (right < s.length()) {
+            // c 是将移入窗口的字符
+            char c = s.charAt(right);
+            // 扩大窗口
+            right++;
+            // 进行窗口内数据的一系列更新
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+            // 判断左侧窗口是否要收缩
+            while (right-left >= t.length()) {
+                // 在这里更新最小覆盖子串
+                if (valid==need.size()) {
+                    return true;
+                }
+                // d 是将移出窗口的字符
+                char d = s.charAt(left);
+                // 缩小窗口
+                left++;
+                // 进行窗口内数据的一系列更新
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.getOrDefault(d,0)-1);
+                }
+            }
+        }
+        return false;
     }
 }
